18c3fe8392292ee221b161fb9bcabfa53789754c
89Explorer <leral3@naver.com>
2024-06-25 13:16:35 +0900

    Initial Commit

---
+++ WhereToGo_Honam/Controllers/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereToGo_Honam
+//
+//  Created by 권정근 on 6/25/24.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBrown
+    }
+
+
+}
+
